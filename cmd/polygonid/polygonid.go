f8b8ad00682469e889985acafd6b3e6e9d083eaa
Oleg Lomaka <oleg.lomaka@gmail.com>
2022-10-28 04:07:33 +0200

    new function PLGNAuthV2InputsMarshal

--- cmd/polygonid/polygonid.go
+++ cmd/polygonid/polygonid.go
@@ -1,3 +1,95 @@
 package main
 
+/*
+#include <stdlib.h>
+
+typedef enum
+{
+	PLGNSTATUSCODE_ERROR,
+	PLGNSTATUSCODE_NIL_POINTER,
+} PLGNStatusCode;
+
+typedef struct _PLGNStatus
+{
+	PLGNStatusCode status;
+	char *error_msg;
+} PLGNStatus;
+*/
+import "C"
+
+import (
+	"encoding/json"
+	"fmt"
+	"unsafe"
+
+	"github.com/iden3/go-circuits"
+)
+
+func maybeCreateStatus(status **C.PLGNStatus, code C.PLGNStatusCode,
+	msgFmt string, msgParams ...interface{}) {
+
+	if status == nil {
+		return
+	}
+
+	s := (*C.PLGNStatus)(C.malloc(C.sizeof_PLGNStatus))
+	if s == nil {
+		return
+	}
+
+	if msgFmt != "" {
+		msg := fmt.Sprintf(msgFmt, msgParams...)
+		s.error_msg = C.CString(msg)
+	} else {
+		s.error_msg = nil
+	}
+
+	s.status = code
+
+	*status = s
+}
+
+//export PLGNAuthV2InputsMarshal
+func PLGNAuthV2InputsMarshal(jsonResponse **C.char, in *C.char,
+	status **C.PLGNStatus) bool {
+
+	if jsonResponse == nil {
+		maybeCreateStatus(status, C.PLGNSTATUSCODE_NIL_POINTER,
+			"jsonResponse pointer is nil")
+		return false
+	}
+
+	in2 := C.GoString(in)
+	var inputs circuits.AuthV2Inputs
+	err := json.Unmarshal([]byte(in2), &inputs)
+	if err != nil {
+		maybeCreateStatus(status, C.PLGNSTATUSCODE_ERROR,
+			"PLGNAuthV2InputsMarshal: inputs unmarshal error: %v", err)
+		return false
+	}
+
+	circuitInputJSON, err := inputs.InputsMarshal()
+	if err != nil {
+		maybeCreateStatus(status, C.PLGNSTATUSCODE_ERROR,
+			"PLGNAuthV2InputsMarshal: inputs marshal error: %v", err)
+		return false
+	}
+
+	*jsonResponse = C.CString(string(circuitInputJSON))
+	return true
+}
+
+//export PLGNFreeStatus
+func PLGNFreeStatus(status *C.PLGNStatus) {
+	if status == nil {
+		return
+	}
+
+	if status.error_msg != nil {
+		C.free(unsafe.Pointer(status.error_msg))
+	}
+
+	C.free(unsafe.Pointer(status))
+}
+
 func main() {}
--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/0xPolygonID/c-polygonid
 
 go 1.19
+
+require github.com/iden3/go-circuits v1.0.1-alpha.2.0.20221026075652-4ae563b63b01
+
+require (
+	github.com/dchest/blake512 v1.0.0 // indirect
+	github.com/iden3/go-iden3-core v0.1.1-0.20221021120133-b70ee94bd9a7 // indirect
+	github.com/iden3/go-iden3-crypto v0.0.13 // indirect
+	github.com/iden3/go-merkletree-sql/v2 v2.0.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
+
+// replace github.com/iden3/go-circuits => ../go-circuits
