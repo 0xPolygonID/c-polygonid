a3d4abf233c08b341611d2c743a2a24a88f26edb
Vladyslav Munin <vladcisco.vm@gmail.com>
2026-03-11 17:11:21 +0200

    add FullVerify

--- auth.go
+++ auth.go
@@ -118,6 +118,93 @@ func VerifyAuthResponse(ctx context.Context, cfg EnvConfig, in []byte) (protocol
 			fmt.Errorf("failed to create auth verifier: %w", err)
 	}
 
+	var authResp *protocol.AuthorizationResponseMessage
+	err = json.Unmarshal(p.AuthResponse, &authResp)
+	if err != nil {
+		return protocol.AuthorizationResponseMessage{},
+			fmt.Errorf("failed to unmarshal auth response: %w", err)
+	}
+
+	err = verifier.VerifyAuthResponse(
+		ctx, *authResp, p.AuthRequest, verificationOptions...)
+	if err != nil {
+		return protocol.AuthorizationResponseMessage{},
+			fmt.Errorf("failed to verify auth response: %w", err)
+	}
+
+	return *authResp, nil
+}
+
+// FullVerify verifies an authorization response using the provided environment configuration and token
+func FullVerify(ctx context.Context, cfg EnvConfig, in []byte) (protocol.AuthorizationResponseMessage, error) {
+	var p proofVerification
+	err := json.Unmarshal(in, &p)
+	if err != nil {
+		return protocol.AuthorizationResponseMessage{},
+			fmt.Errorf("failed to unmarshal proof verification: %w", err)
+	}
+
+	// get cache directory
+	StateCacheOptions, RootCacheOptions, releaseCache, err :=
+		provideCacheLayer(cfg.CacheDir)
+	if err != nil {
+		return protocol.AuthorizationResponseMessage{},
+			fmt.Errorf("failed to provide cache layer: %w", err)
+	}
+	defer releaseCache()
+
+	resolvers := make(map[string]pubsignals.StateResolver, len(cfg.ChainConfigs))
+	for chainID, chainCfg := range cfg.ChainConfigs {
+		resolver, err := state.NewETHResolverWithOpts(
+			chainCfg.RPCUrl,
+			chainCfg.StateContractAddr.Hex(),
+			state.WithStateCacheOptions(&StateCacheOptions),
+			state.WithRootCacheOptions(&RootCacheOptions),
+		)
+		if err != nil {
+			return protocol.AuthorizationResponseMessage{},
+				fmt.Errorf("failed to create ETH state resolver for chainID %v: %w", chainID, err)
+		}
+		c, n, err := core.NetworkByChainID(chainID)
+		if err != nil {
+			return protocol.AuthorizationResponseMessage{},
+				fmt.Errorf("failed to get network for chainID %v: %w", chainID, err)
+		}
+		resolvers[fmt.Sprintf("%s:%s", c, n)] = resolver
+	}
+
+	verificationOptions := []pubsignals.VerifyOpt{}
+	if p.Options.AcceptedStateTransitionDelay != "" {
+		d, err := time.ParseDuration(p.Options.AcceptedStateTransitionDelay)
+		if err != nil {
+			return protocol.AuthorizationResponseMessage{},
+				fmt.Errorf("failed to parse accepted state transition delay: %w", err)
+		}
+		o := pubsignals.WithAcceptedStateTransitionDelay(d)
+		verificationOptions = append(verificationOptions, o)
+	}
+	if p.Options.AcceptedProofGenerationDelay != "" {
+		d, err := time.ParseDuration(p.Options.AcceptedProofGenerationDelay)
+		if err != nil {
+			return protocol.AuthorizationResponseMessage{},
+				fmt.Errorf("failed to parse accepted proof generation delay: %w", err)
+		}
+		o := pubsignals.WithAcceptedProofGenerationDelay(d)
+		verificationOptions = append(verificationOptions, o)
+	}
+
+	keyloader := loaders.NewEmbeddedKeyLoader()
+	opts := []auth.VerifierOption{
+		auth.WithDocumentLoader(cfg.documentLoader()),
+		auth.WithStateVerificationOpts(verificationOptions...),
+	}
+
+	verifier, err := auth.NewVerifier(keyloader, resolvers, opts...)
+	if err != nil {
+		return protocol.AuthorizationResponseMessage{},
+			fmt.Errorf("failed to create auth verifier: %w", err)
+	}
+
 	if err := verifier.SetPacker(&packers.PlainMessagePacker{}); err != nil {
 		return protocol.AuthorizationResponseMessage{},
 			fmt.Errorf("failed to set plain message packer: %w", err)
